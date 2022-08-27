70f2ec6c540fa179cb88569ccfa9bc717c90e65b
ana4is <anaisflechier@gmail.com>
2022-08-27 08:20:57 +0200

    Install React + Bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button type="button" class="btn btn-info">
+          Info
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
